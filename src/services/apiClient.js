3372288143761c377f3ff2786ce3087c6aaa33a2
Patricio Cordoba <patricio.cordoba@parserx.com>
2022-11-15 17:00:22 -0300

    feat: fixed bad apiURl

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://taller-prog-2-stock.herokuapp.com/"
+const BASE_URL = "https://taller-prog-2-stock.herokuapp.com"
 
 const pageSize = 5;
 const historyPageSize = 10;
